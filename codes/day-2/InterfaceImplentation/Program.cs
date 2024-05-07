1c72a2b81037f5dd9e885c87c195edd39918adaf
Joydip <joydip.mondal@gmail.com>
2024-05-07 17:33:06 +0530

    Day-2 final upload

---
+++ codes/day-2/InterfaceImplentation/Program.cs
@@ -0,0 +1,71 @@
+﻿//IBankService? service = Factory.Instantiate.CreateService<IBankService,BankService>();
+//if (service != null) {
+//    BankOperator bankOperator = new BankOperator(service);
+//    bankOperator.TransferAmount(1000, 1123, 1234);
+//}
+
+IBankService service = Factory.Instantiate.CreateService();
+if (service != null)
+{
+    BankOperator bankOperator = new BankOperator(service);
+    bankOperator.TransferAmount(1000, 1123, 1234);
+}
+
+//Factory
+class Factory
+{
+    private static Factory? _factory;
+
+    private Factory() { }
+
+    public static Factory Instantiate
+    {
+        get
+        {
+            _factory ??= new Factory();
+            return _factory;
+        }
+    }
+
+    public IBankService CreateService() => new BankService();
+    //public TInterface? CreateService<TInterface, TClass>()
+    //{
+    //    return (TInterface?)Activator.CreateInstance(typeof(TClass));
+    //}
+}
+
+//contract
+interface IBankService
+{
+    public abstract void CreditAmountToAccount(decimal amount, int creditAccId);
+    public abstract void DebitAmountFromAccount(decimal amount, int debitAccId);
+}
+
+//implementation
+class BankService : IBankService
+{
+    public void CreditAmountToAccount(decimal amount, int creditAccId)
+    {
+
+    }
+
+    public void DebitAmountFromAccount(decimal amount, int debitAccId)
+    {
+
+    }
+}
+
+class BankOperator
+{
+    private readonly IBankService bankService;
+    public BankOperator(IBankService bankService) => this.bankService = bankService;
+
+    public void TransferAmount(decimal amount, int debitAccId, int creditAccId)
+    {
+        //IBankService bankService = new BankService();
+        bankService.DebitAmountFromAccount(amount, debitAccId);
+        bankService.CreditAmountToAccount(amount, creditAccId);
+    }
+}
+
+
